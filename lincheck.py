43be6a3bdf87959bdd2f790ced4dca8a597d7f3d
perseu <jmsaguas@gmail.com>
2023-08-24 12:16:50 +0200

    And so it begins!

---
+++ lincheck.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 24 12:09:26 2023
+
+@author: jmsaguas
+
+"""
+
+# imports
+import os, sys
+
+
+# Global Variables
+pathImages = ''
+pathTargetRegions = ''
+
+
+# Functions
+
+
+# If there was a Main, this would be it...
